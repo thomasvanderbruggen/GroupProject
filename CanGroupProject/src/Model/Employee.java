8b382beb8257a906d79e6f8615523b3392c2b721
thomasvanderbruggen <66325982+thomasvanderbruggen@users.noreply.github.com>
2021-03-07 18:40:42 -0600

    Annotated Employee

--- CanGroupProject/src/Model/Employee.java
+++ CanGroupProject/src/Model/Employee.java
@@ -1,5 +1,42 @@
 package Model;
 
-public class Employee {
+import java.time.LocalDate;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name="employees")
+public class Employee {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="id") 
+	private int id; 
+	@Column(name="employee_name")
+	private String name; 
+	@Column(name="hire_date") 
+	private LocalDate hireDate;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public LocalDate getHireDate() {
+		return hireDate;
+	}
+	public void setHireDate(LocalDate hireDate) {
+		this.hireDate = hireDate;
+	}
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", name=" + name + ", hireDate=" + hireDate + "]";
+	} 
+	
+	
 }
